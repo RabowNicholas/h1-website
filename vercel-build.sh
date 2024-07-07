dd1e4db71d016fc51c0d062a3ad2ffb46b0c3acc
Nicholas Rabow <rabownick123@gmail.com>
2024-07-07 15:25:00 -0600

    remove sudo from script

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Install Git LFS
-echo "Installing Git LFS"
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
+# Download and install Git LFS
+echo "Downloading Git LFS"
+curl -s https://github.com/git-lfs/git-lfs/releases/download/v3.2.0/git-lfs-linux-amd64-v3.2.0.tar.gz -L -o git-lfs.tar.gz
+
+echo "Extracting Git LFS"
+tar -xzf git-lfs.tar.gz
+cd git-lfs-3.2.0
+./install.sh
 
 # Initialize and pull LFS files
 echo "Initializing Git LFS"
